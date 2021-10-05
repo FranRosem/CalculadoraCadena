05396311e3bd6c55300cb534ca998859b397eb35
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-10-05 01:49:26 -0400

    La suma ahora recibe 1 delimitador personalizado

--- src/CalculadoraTotalizador.test.js
+++ src/CalculadoraTotalizador.test.js
@@ -5,12 +5,19 @@ describe("String Calculator ", () => {
     it("deberia retornar la suma de un string con coma y un guión", () => {
         expect(sumar("1-2,3")).toEqual(6);
     });
+    it("deberia retornar la suma de un string con delimitador personalizado", () => {
+        expect(sumar("//;\n1;2;3")).toEqual(6);
+    });
   });
   
   function sumar(stringNumbers) {
-    const numbers = stringNumbers
-        .split(/,|-/)
-        .map(n => parseInt(n, 10))
-    return numbers.reduce((s, n) => s + n)
+    var delimeter = ',';
+    if (stringNumbers.substring(0, 2) === '//') {
+      delimeter = stringNumbers.substring(2, stringNumbers.indexOf('\n'));
+      stringNumbers = stringNumbers.substring(stringNumbers.indexOf('\n') + 1);
+    }
+    return stringNumbers.replace(/(\n|-)/gm, delimeter).split(delimeter).reduce(function(previous, current) {
+      return parseInt(previous) + parseInt(current);
+    }, 0);
   }
   
