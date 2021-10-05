99f8a73d655c31bc64706568e4b4e7190c8aaf49
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-10-05 01:54:20 -0400

    Ignora números mayores a 1000

--- src/CalculadoraTotalizador.test.js
+++ src/CalculadoraTotalizador.test.js
@@ -8,6 +8,12 @@ describe("String Calculator ", () => {
     it("deberia retornar la suma de un string con delimitador personalizado", () => {
         expect(sumar("//;\n1;2;3")).toEqual(6);
     });
+    it("deberia retornar la suma de un string con delimitador personalizado con más longitud", () => {
+        expect(sumar("//***\n1***2***3")).toEqual(6);
+    });
+    it("deberia ignorar números mayores a 1000", () => {
+        expect(sumar("//***\n1***2***3000")).toEqual(3);
+    });
   });
   
   function sumar(stringNumbers) {
@@ -16,7 +22,9 @@ describe("String Calculator ", () => {
       delimeter = stringNumbers.substring(2, stringNumbers.indexOf('\n'));
       stringNumbers = stringNumbers.substring(stringNumbers.indexOf('\n') + 1);
     }
-    return stringNumbers.replace(/(\n|-)/gm, delimeter).split(delimeter).reduce(function(previous, current) {
+    return stringNumbers.replace(/(\n|-)/gm, delimeter).split(delimeter).filter(function(element) {
+        return element <= 1000;
+      }).reduce(function(previous, current) {
       return parseInt(previous) + parseInt(current);
     }, 0);
   }
