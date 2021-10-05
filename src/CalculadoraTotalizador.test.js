f6c0c683f1d432925422ba14474b7daad325b873
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-10-05 00:24:33 -0400

    Recibir y transformar la cadena a una suma de enteros separados por una coma y guion

--- src/CalculadoraTotalizador.test.js
+++ src/CalculadoraTotalizador.test.js
@@ -2,12 +2,16 @@ describe("String Calculator ", () => {
     it("deberia retornar la suma de un string con coma", () => {
         expect(sumar("1,2,3")).toEqual(6);
       });
+    it("deberia retornar la suma de un string con coma y un guión", () => {
+        expect(sumar("1-2,3")).toEqual(6);
+    });
   });
   
   function sumar(stringNumbers){
     const numbers = stringNumbers
-      .split(',')
-      .map(n => parseInt(n, 10))
+        .replace('-', ',')
+        .split(',')
+        .map(n => parseInt(n, 10))
     return numbers.reduce((s, n) => s + n)
   }
   
